cc86b0c75ac989851b1a0b59a0daafdc0de593f1
Ramin <raminrocker75@gmail.com>
2021-05-25 18:23:45 -0500

    things we have done so far

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,10 @@
 public class BranchingPractice {
     public static void main(String[] args) {
         System.out.println("hello world");
+//        created new project
+//        Enabled version control
+//        added .gitignore with .idea , target folder and *.iml file
+//        made initial commit by keeping track of all files
+//        shared project on github
     }
 }
